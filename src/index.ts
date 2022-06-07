8390ab8b0a5de558bf9abcb5ac0b57fee932b5e4
PabloKazalukian <kazalukianpablo@gmail.com>
2022-06-07 20:59:58 -0300

    cors

--- src/index.ts
+++ src/index.ts
@@ -28,7 +28,9 @@ router.use(bodyParser.json());
 /** Rules of our API */
 router.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
 
     if (req.method == 'OPTIONS') {
         res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
