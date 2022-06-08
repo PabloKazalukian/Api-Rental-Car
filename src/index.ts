fd464e671de75a7b35fd415fec27ae58e80445ae
PabloKazalukian <kazalukianpablo@gmail.com>
2022-06-08 11:12:40 -0300

    cors

--- src/index.ts
+++ src/index.ts
@@ -28,7 +28,7 @@ router.use(bodyParser.json());
 /** Rules of our API */
 router.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Headers', '*');
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
 
     // if (req.method == 'OPTIONS') {
