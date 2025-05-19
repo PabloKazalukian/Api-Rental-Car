63de9e236bd49c5b8211553a668171307c7ab4d3
pablo <kazalukianpablo@gmail.com>
2025-05-19 18:31:58 -0300

    dotenv0

---
+++ src/types/express/index.d.ts
@@ -0,0 +1,10 @@
+import { UserEntity } from '../../entities/user.entity';
+
+declare global {
+    namespace Express {
+        interface User extends UserEntity { }
+        interface Request {
+            user?: User;
+        }
+    }
+}
