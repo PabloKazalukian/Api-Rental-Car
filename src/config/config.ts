6b42cf0afd2fb5ad1c957cab53b07bf79614e5b7
pablo <kazalukianpablo@gmail.com>
2025-05-19 16:12:03 -0300

    types

--- src/config/config.ts
+++ src/config/config.ts
@@ -23,35 +23,3 @@ export abstract class ConfigServer {
         return AppDataSource.initialize(); // Lo conecta si aún no lo está
     }
 }
-
-
-// dotenv.config();
-
-// const MYSQL_HOST = process.env.DB_HOST;
-// const MYSQL_DATABASE = process.env.DB_DATABASE;
-// const MYSQL_USER = process.env.DB_USER;
-// const MYSQL_PASS = process.env.DB_PASSWORD;
-// const MYSQL_PORT = process.env.DB_PORT;
-
-
-// const MYSQL = {
-//     host: MYSQL_HOST,
-//     database: MYSQL_DATABASE,
-//     user: MYSQL_USER,
-//     pass: MYSQL_PASS,
-//     port: MYSQL_PORT
-// };
-
-// const AUTH_JWT: string = process.env.AUTH_JWT || "SECRETO";
-
-// const AUTH = {
-//     key: AUTH_JWT,
-//     expires: "24h",
-// };
-
-// const config = {
-//     mysql: MYSQL,
-//     auth: AUTH,
-// };
-
-// export default config;
